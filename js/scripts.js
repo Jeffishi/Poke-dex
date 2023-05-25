e0e759a576d946df6de8f12280273f828b1c8a0c
Jeffishi <arsenefriend@gmail.com>
2023-05-25 13:06:23 -0500

    updates to pokedex folder
    created new folder and populated folders

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+alert('Hello world');
+
+let favoriteFood = 'rice&Stew';
+document.write(favoriteFood);
+favoriteFood = 'Steak&Veggies';
+document.write(favoriteFood);
+
+let simpleAddition=2+2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize); 
+let miniSize = (doubleSize*2) - (size/2);
+document.write(miniSize);
