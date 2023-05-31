4dd5ec5ed3080996e0f0352b4f7a3ec06b3c1475
Jeffishi <arsenefriend@gmail.com>
2023-05-31 11:57:31 -0500

    Created a new variable called pokemonList
    added several objects to the array. Each object represents one Pokémon and needs to have the same keys.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'rice&Stew';
-document.write(favoriteFood);
-favoriteFood = 'Steak&Veggies';
-document.write(favoriteFood);
-
-let simpleAddition=2+2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize); 
-let miniSize = (doubleSize*2) - (size/2);
-document.write(miniSize);
+let pokemonList=  [ //database of pokemon for pokedex//
+        {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'], HP: 45 },
+        {name: 'Charmander', height: 0.6, type: ['Fire'], HP: 39 },
+        {name: 'Squirtle', height: 0.5, type: ['Water'], HP: 44},
+        {name: 'Abra', height: 0.9, type: ['psychic'], HP: 25}];
