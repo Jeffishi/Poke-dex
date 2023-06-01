26f5b3a48245967557df0561918f22ac9f34e0cf
Jeffishi <arsenefriend@gmail.com>
2023-06-01 18:53:45 -0500

    Added for loops and conditions
    added a for loop listing all names and heights of pokemons. added categories to each based on sizes. (1.3)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,41 @@
-let pokemonList=  [ //database of pokemon for pokedex//
-        {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'], HP: 45 },
-        {name: 'Charmander', height: 0.6, type: ['Fire'], HP: 39 },
-        {name: 'Squirtle', height: 0.5, type: ['Water'], HP: 44},
-        {name: 'Abra', height: 0.9, type: ['psychic'], HP: 25}];
+let pokemonList = [
+  //database of pokemon for pokedex//
+  { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"], HP: 45 },
+  { name: "Charmander", height: 0.6, type: ["Fire"], HP: 39 },
+  { name: "Squirtle", height: 0.5, type: ["Water"], HP: 44 },
+  { name: "Abra", height: 0.9, type: ["psychic"], HP: 25 },
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height <= 0.5) {
+    document.write(
+      pokemonList[i].name +
+        " " +
+        "(Height: " +
+        pokemonList[i].height +
+        ")" +
+        " -That's a small size pokemon! " +
+        "<br><br>"
+    );
+  } else if (pokemonList[i].height > 0.5 && pokemonList[i].height <= 0.8) {
+    document.write(
+      pokemonList[i].name +
+        " " +
+        "(Height: " +
+        pokemonList[i].height +
+        ")" +
+        " -That's an avergae size pokemon! " +
+        "<br><br>"
+    );
+  } else {
+    document.write(
+      pokemonList[i].name +
+        " " +
+        "(Height: " +
+        pokemonList[i].height +
+        ")" +
+        " -That's a Big pokemon! " +
+        "<br><br>"
+    );
+  }
+}
